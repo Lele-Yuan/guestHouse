783a927d17a50f37ad0058200aac4380b71131e2
yuanlele <lele@leledeMacBook-Pro.local>
2021-06-08 12:24:16 +0800

    🚀 feat: 增加家具列表逻辑

--- miniprogram/components/furnitureList/furnitureList.js
+++ miniprogram/components/furnitureList/furnitureList.js
@@ -11,7 +11,32 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    listData: [{
+      icon: 'beds',
+      list: [{
+        name: '双人床 1.5m*2m',
+        count: 1,
+        unit: '张'
+      },{
+        name: '双人床 1.8m*2m',
+        count: 1,
+        unit: '张'
+      }]
+    },{
+      icon: 'bed',
+      list: [{
+        name: '单人床 1.2m*2m',
+        count: 1,
+        unit: '张'
+      }]
+    },{
+      icon: 'bed',
+      list: [{
+        name: '榻榻米 1.2m*2m',
+        count: 1,
+        unit: '张'
+      }]
+    }]
   },
 
   /**
